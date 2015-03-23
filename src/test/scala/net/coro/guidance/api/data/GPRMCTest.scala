34ec0528315cb872a1bd4994d130536a60ef8b5b
condon.andy@gmail.com <andrew.condon@echostar.com>
2015-03-23 15:09:07 -0600

    Better still

--- src/test/scala/net/coro/guidance/api/data/GPRMCTest.scala
+++ src/test/scala/net/coro/guidance/api/data/GPRMCTest.scala
@@ -6,13 +6,13 @@ import net.coro.guidance.api.data.gps.GPRMC
 class GPRMCTest extends TestSpec {
   "The location must parse correctly from a GPRMC sentence" in {
     val gprmc = GPRMC.fromSentence("$GPRMC,034444.000,A,4110.1833,N,10449.5843,W,1.50,296.10,200315,,,A*7D")
-    val expectedLocation = Location(Angle(North, 41, 10, 10.998f), Angle(West, 104, 49, 35.058f))
+    val expectedLocation = Location(Angle(North, 41, 10.1833), Angle(West, 104, 49.5843))
 
     gprmc.location mustBe expectedLocation
   }
 
   "The location must convert to Google Maps" in {
-    val location = Location(Angle(North, 41, 10, 10.998f), Angle(West, 104, 49, 35.058f))
+    val location = Location(Angle(North, 41, 10.1833), Angle(West, 104, 49.5843))
 
     location.toGmaps mustBe "41.16972166670693,-104.826404999627"
   }
